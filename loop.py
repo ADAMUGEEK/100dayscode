896a3eb47d491c0b45c243c24119a4fcbb277e40
ADAMUGEEK <57603839+ADAMUGEEK@users.noreply.github.com>
2021-01-09 21:12:22 +0100

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,2 +1,3 @@
+#loop instruction used to ask user number and loops to accept the input base on the number
 for i in range ( int(input(" enter number of times to print hello world:"))):
     input("enter the value of  x :")
