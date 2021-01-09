3ad8257daa2131424068014eea9d8b3f2803e832
ADAMUGEEK <57603839+ADAMUGEEK@users.noreply.github.com>
2021-01-09 21:09:33 +0100

    Add files via upload

---
+++ daysone.py
@@ -0,0 +1,1 @@
+print("Hello  World of Programming  " + input(" Welcome to Python Please to Whom i am talk to :"))
---
+++ loop.py
@@ -0,0 +1,2 @@
+for i in range ( int(input(" enter number of times to print hello world:"))):
+    input("enter the value of  x :")
