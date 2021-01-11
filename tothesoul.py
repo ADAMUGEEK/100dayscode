9af7a9dbb708d9cbd23dd702b7d9d0c84cf849bc
ADAMUGEEK <57603839+ADAMUGEEK@users.noreply.github.com>
2021-01-11 08:01:28 +0100

    Add files via upload

---
+++ tothesoul.py
@@ -0,0 +1,12 @@
+print("\t \t\t welcome feel free to enjoy staying here \n")
+name = input(" \t please to whom am chatting with if you dont maind what is your name \n")
+age = (input(f" please  {name} enter your age  let discover something : \n") )
+age_int = int(age)
+age_days = age_int * 365
+age_week = age_int  * 52
+age_mouth = age_int * 12
+massage = f" {name} did you know that you have {age_mouth} mouth, \n \t\t\t\t {age_week} weeks \n \t\t\t\t {age_days} days \t\t \n \t in this world approximatly"
+print(massage)
+proud =f"think all this time what are pround of {name}? \n did you did you learn QURAN and understand it ? \n rember the first revalation to the  holy prophet \n \t \t \t READ \n \tREAD IN NAME OF ALLAH ... "
+print(proud)       
+          
