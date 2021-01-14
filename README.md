53bf6237b2fd7c325d551ca50337b59a47a33e0b
ADAMUGEEK <muhdadamu0008@gmail.com>
2021-01-14 22:19:43 +0100

    becareful unlike in c++ to used (or and) u  most mention the variable at each comparising

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 100dayscode
 python practice file
+this repository was clone but in vs code and pycharm
+am push but from vs code and pycharm
--- vcode.py
+++ vcode.py
@@ -1,17 +1,27 @@
 import pyttsx3
 if __name__ == '__main__':
-    engine = pyttsx3.init()  # object creation
+    engine = pyttsx3.init()  # object creation no need in pycharm
     welcome = "\t\t\t welcome to geek innovative technology"
+    my_name = "CEO"
 
     def speak(text):
         engine.say(text)
         engine.runAndWait()
         engine.stop()
-    speak("welcome")
-
+    speak(welcome)
+    
+    user = ""
     print(welcome)
-    user = input(speak("what is your name please"))
-    speak(f"hello {user} how may i help you")
+    user = str(input(speak("what is your name please")))
+    if user == "adamu" or user == "ADAMU":
+        speak("welcome back my dear may Allah bostowed orpan you door's of success and grant you his mercy ameen ameen ameen" )
+        if user == "adamu":
+            speak(f"yea let do it {my_name} we are all set")
+        elif user == "ADAMU":
+            speak(f"sorry to interrupt but the keyboard is in cap")
+    else:
+        speak(f"hello {user} how may i help you")
+
     
 
 
