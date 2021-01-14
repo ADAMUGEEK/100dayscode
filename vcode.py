48429ac669ab897ee7473ad7fd556ea653f2b695
ADAMUGEEK <muhdadamu0008@gmail.com>
2021-01-14 21:17:21 +0100

    speak by geeky

--- vcode.py
+++ vcode.py
@@ -1,1 +1,14 @@
-print("welcome to vs code")
+import pyttsx3
+if __name__ == '__main__':
+    engine = pyttsx3.init()  # object creation
+
+    def speak(text):
+        engine.say(text)
+        engine.runAndWait()
+        engine.stop()
+    speak("welcome")    
+    
+
+
+
+
