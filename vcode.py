aeaff62a9b63f061ce0e7c40b6724fefd009b15b
ADAMUGEEK <muhdadamu0008@gmail.com>
2021-01-14 21:26:09 +0100

    yea now a get it

--- vcode.py
+++ vcode.py
@@ -1,12 +1,17 @@
 import pyttsx3
 if __name__ == '__main__':
     engine = pyttsx3.init()  # object creation
+    welcome = "\t\t\t welcome to geek innovative technology"
 
     def speak(text):
         engine.say(text)
         engine.runAndWait()
         engine.stop()
-    speak("welcome")    
+    speak("welcome")
+
+    print(welcome)
+    user = input(speak("whow are you please"))
+    speak(f"hello {user} how may i help you")
     
 
 
